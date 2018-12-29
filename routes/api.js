6c2a610bf39951bb33bb0f6bde33863f5358968d
ramkrishnan <iramkrish95@gmail.com>
2018-12-29 19:45:00 +0530

    routes for different crud operations

---
+++ routes/api.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const movie = require('../models/mongo-model');
+
+// get list of ninjas from database
+router.get('/movie',async (req,res,next)=>{
+    const getResponse = await movie.find();
+    res.send(getResponse);
+});
+
+// add a ninja to database
+router.post('/movie',async (req,res,next) => {
+
+    try
+    {const Movie = new movie(req.body);
+    await Movie.save();
+    res.send(Movie);}
+    catch(err){
+        next(err);
+    }
+
+});
+
+// update a ninja from database
+router.put('/movie/:id',async (req,res,next)=>{
+    try {
+    await movie.findByIdAndUpdate({ _id:req.params.id },req.body);
+    const putResponse = await movie.findOne({_id:req.params.id});
+    res.send(putResponse);}
+    catch(err){
+        next(err);
+    }
+});
+
+// delete a ninja from database
+router.delete('/movie/:id',async(req,res,next)=>{
+    try{
+    const deleteResponse = await movie.findByIdAndRemove({_id:req.params.id});
+    res.send(deleteResponse);}
+    catch(err){
+        next(err);
+    }
+});
+
+module.exports = router;

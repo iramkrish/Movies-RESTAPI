40464c9d2f0fbf6e3b72ed425ea3c1dfc118862c
iramkrish <iramkrish95@gmail.com>
2018-12-30 09:19:17 +0530

    updated comments

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const movie = require('../models/mongo-model');
 
-// get list of ninjas from database
+// get list of movies from database
 router.get('/movie',async (req,res,next)=>{
     const getResponse = await movie.find();
     res.send(getResponse);
 });
 
-// add a ninja to database
+// add a movie to database
 router.post('/movie',async (req,res,next) => {
 
     try
@@ -21,7 +21,7 @@ router.post('/movie',async (req,res,next) => {
 
 });
 
-// update a ninja from database
+// update a movie from database
 router.put('/movie/:id',async (req,res,next)=>{
     try {
     await movie.findByIdAndUpdate({ _id:req.params.id },req.body);
@@ -32,7 +32,7 @@ router.put('/movie/:id',async (req,res,next)=>{
     }
 });
 
-// delete a ninja from database
+// delete a movie from database
 router.delete('/movie/:id',async(req,res,next)=>{
     try{
     const deleteResponse = await movie.findByIdAndRemove({_id:req.params.id});
